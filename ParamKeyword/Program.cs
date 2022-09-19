0c373138c4aae1a7478ff0bd44fda09dba888755
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-19 16:47:30 +0530

    Add project files.

---
+++ ParamKeyword/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamKeyword
+{   
+  
+            internal class Program
+            {
+            static void AcceptNames(params string[] names)
+            {
+                for (int i = 0; i < names.Length; i++)
+                {
+                    Console.WriteLine(names[i]);
+               
+                }
+            Console.WriteLine("---------------------");
+        }
+            static void Main(string[] args)
+            {
+                AcceptNames("Rahul", "Shubhangi", "Shubham");
+                AcceptNames("Rahul", "Shubhangi");
+                AcceptNames("Rahul", "Shubhangi", "Shubham", "Mahesh");
+            }
+
+        
+
+            }
+}
+
